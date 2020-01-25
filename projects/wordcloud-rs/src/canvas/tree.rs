3fa44668fb82f22f27106fdc8bc1ecb219ad65fc
Aster <aster@vers.site>
2020-01-25 09:33:58 +0800

    ♻️ Replace quadtree with `kdtree` library

--- projects/wordcloud-rs/src/canvas/tree.rs
+++ projects/wordcloud-rs/src/canvas/tree.rs
@@ -30,17 +30,64 @@ impl QuadTree {
             if self.children.is_empty() {
                 if other.children.is_empty() {
                     return true;
-                }
-                else {
+                } else {
                     other.children.iter().any(|ct| self.overlaps(ct, x1, y1, x2, y2))
                 }
-            }
-            else {
+            } else {
                 self.children.iter().any(|ct| other.overlaps(ct, x2, y2, x1, y1))
             }
-        }
-        else {
+        } else {
             return false;
         }
     }
+}
+
+use kdtree::KdTree;
+use kdtree::ErrorKind;
+use kdtree::distance::squared_euclidean;
+
+#[test]
+fn test() {
+    let a: ([f64; 2], usize) = ([0f64, 0f64], 0);
+    let b: ([f64; 2], usize) = ([1f64, 1f64], 1);
+    let c: ([f64; 2], usize) = ([2f64, 2f64], 2);
+    let d: ([f64; 2], usize) = ([3f64, 3f64], 3);
+
+    let dimensions = 2;
+    let mut kdtree = KdTree::new(dimensions);
+
+    kdtree.add(&a.0, a.1).unwrap();
+    kdtree.add(&b.0, b.1).unwrap();
+    kdtree.add(&c.0, c.1).unwrap();
+    kdtree.add(&d.0, d.1).unwrap();
+
+    assert_eq!(kdtree.size(), 4);
+    assert_eq!(
+        kdtree.nearest(&a.0, 0, &squared_euclidean).unwrap(),
+        vec![]
+    );
+    assert_eq!(
+        kdtree.nearest(&a.0, 1, &squared_euclidean).unwrap(),
+        vec![(0f64, &0)]
+    );
+    assert_eq!(
+        kdtree.nearest(&a.0, 2, &squared_euclidean).unwrap(),
+        vec![(0f64, &0), (2f64, &1)]
+    );
+    assert_eq!(
+        kdtree.nearest(&a.0, 3, &squared_euclidean).unwrap(),
+        vec![(0f64, &0), (2f64, &1), (8f64, &2)]
+    );
+    assert_eq!(
+        kdtree.nearest(&a.0, 4, &squared_euclidean).unwrap(),
+        vec![(0f64, &0), (2f64, &1), (8f64, &2), (18f64, &3)]
+    );
+    assert_eq!(
+        kdtree.nearest(&a.0, 5, &squared_euclidean).unwrap(),
+        vec![(0f64, &0), (2f64, &1), (8f64, &2), (18f64, &3)]
+    );
+    assert_eq!(
+        kdtree.nearest(&b.0, 4, &squared_euclidean).unwrap(),
+        vec![(0f64, &1), (2f64, &0), (2f64, &2), (8f64, &3)]
+    );
 }
