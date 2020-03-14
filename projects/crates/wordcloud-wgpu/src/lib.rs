a0e786b65abb1853c7458ba82b5b8d3d81fe13a2
aster <aster@vers.site>
2020-03-14 12:15:09 +0800

    🔥 Remove `wordcloud-wgpu` and `wordcloud-wasi` Rust crates
    TypeScript layout and rendering no longer depend on GPU or WASI components.

--- projects/crates/wordcloud-wgpu/src/lib.rs
+++
@@ -1,6 +0,0 @@
-//! GPU-accelerated word cloud layout (`wgpu` compute).
-
-mod engine;
-
-pub use engine::GpuLayoutEngine;
-pub use wordcloud_types::{LayoutConfig, LayoutResult, WordCloudError, WordCloudResult, WordFrequency};
